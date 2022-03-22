ba84c5f8b06046333db8b2394a075daa54eba9ac
danielamoncada23 <101958908+danielamoncada23@users.noreply.github.com>
2022-03-21 23:24:57 -0600

    Se agrega el proyecto

---
+++ database/factories/DoctorFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Doctor>
+ */
+class DoctorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nombre' => $this->faker->Name,
+            'apellido' => $this->faker->lastName,
+            'especialidad' =>$this->faker->randomElement($array = array ('pediatra','general')),
+            'turno' =>$this->faker->randomElement($array = array ('A','B')),
+            'telefono' => $this->faker->numerify('########'),
+            'correo electronico' =>$this->faker->email,
+
+        ];
+    }
+}
---
+++ database/migrations/2022_03_22_040445_create_salas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salas', function (Blueprint $table) {
+            $table->id();
+            $table->enum('sala', array('C', 'D'));;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salas');
+    }
+};
